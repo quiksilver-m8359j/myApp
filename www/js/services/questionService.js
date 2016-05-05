f96607ad422571f2cb50ad8c5c73c8e3f3cef346
quiksilver-m8359j <bitmonsterx@yahoo.co.jp>
2016-05-05 23:25:15 +0900

    Project's Initial Commit

---
+++ www/js/services/questionService.js
@@ -0,0 +1,31 @@
+// This is a JavaScript file
+
+app.value('questionsService', {
+    questions : [
+            {
+            "text": "iPhoneの主要な開発言語は？",
+            "answer": "Objective-C",
+            "choices": ["Java","JavaScript","Python"]
+          },
+          {
+            "text": "Google社製のWEBブラウザは？",
+            "answer": "Chrome",
+            "choices": ["FireFox","Opera","Safari"]
+          },
+          {
+            "text": "HTTPステータスコードで503の意味は？",
+            "answer": "サーバー過負荷",
+            "choices": ["NotFound","認証エラー","アクセス禁止"]
+          },
+          {
+            "text": "英語のスラングでバカ・間抜けといった意味を持つ分散型バージョン管理システムとは？",
+            "answer": "Git",
+            "choices": ["Subversion","Mercurial","CVS"]
+          },
+          {
+            "text": "HTMLのHはどんな意味？",
+            "answer": "Hyper",
+            "choices": ["Hip","Handshake","History"]
+          }
+        ]
+});
